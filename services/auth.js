bd3d3c2b9ae03c99ceabf7f6859bce4b0d170bad
Vishal Singh <vishalsinghlodhi16@gmail.com>
2025-03-21 14:38:58 +0530

    Secret key error solved

--- services/auth.js
+++ services/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const secret = process.env.JWT_Secret;
+const secret = process.env.JWT_SECRET;
 
 function setuser(user) {
     return jwt.sign({
