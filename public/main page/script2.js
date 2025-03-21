bfb298fb300852ba9a3e127e97d0af0e4829129f
Vishal Singh <vishalsinghlodhi16@gmail.com>
2025-03-21 16:17:13 +0530

    footer updated

--- public/main page/script2.js
+++ public/main page/script2.js
@@ -1,11 +1,11 @@
-function updateClock() {
-    const clock = document.getElementById('clock');
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    clock.textContent = `${hours}.${minutes}.${seconds}`;
-}
+// function updateClock() {
+//     const clock = document.getElementById('clock');
+//     const now = new Date();
+//     const hours = String(now.getHours()).padStart(2, '0');
+//     const minutes = String(now.getMinutes()).padStart(2, '0');
+//     const seconds = String(now.getSeconds()).padStart(2, '0');
+//     clock.textContent = `${hours}.${minutes}.${seconds}`;
+// }
 
-setInterval(updateClock, 1000);
-updateClock(); // Initial call to display the clock immediately
+// setInterval(updateClock, 1000);
+// updateClock(); 
