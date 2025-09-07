54c1c59a98ebbf2fce8d073290756a0a3540de9a
Vishal Singh <vishalsinghlodhi16@gmail.com>
2025-09-08 00:49:15 +0530

    session store warning solving

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const session = require('express-session');
 const flash = require('connect-flash');
 const express = require('express');
-const MongoStore = require('connect-mongo')
 const cookieParser = require('cookie-parser')
 const path = require('path');
 const useRouter = require('./routes/routes');
@@ -38,16 +37,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
 }));
-app.use(session({
-    secret: process.env.JWT_Secret,
-    resave: false,
-    saveUninitialized: false, 
-    store: MongoStore.create({       
-        mongoUrl: process.env.MONGO_URI,
-        collectionName: 'sessions',
-        ttl: 14 * 24 * 60 * 60 
-    })
-}));
+
 app.use(flash());
 
 const socketIo = require("socket.io");
